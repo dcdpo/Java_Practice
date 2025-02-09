06ef7f5a930b4bae9780ca5efb950caa7a507884
dcdpo <sodagreenapay@gmail.com>
2025-02-09 22:55:45 +0800

    2/9 練習

---
+++ src/ch2/Ch2_2.java
@@ -0,0 +1,10 @@
+package ch2;
+
+public class Ch2_2 {
+    // main是程式的起點
+    public static void main(String[] args) {
+        // 字串輸出
+        System.out.println("Hello! Java");
+        System.out.println("I Love Java");  // 第2筆字串輸出
+    }
+}
