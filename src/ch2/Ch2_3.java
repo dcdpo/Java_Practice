74cc7d5281df0d4269bc3b7a714ff7d18f11e459
dcdpo <sodagreenapay@gmail.com>
2025-02-17 06:06:04 +0800

    2/17 2-3、2-4練習

---
+++ src/ch2/Ch2_3.java
@@ -0,0 +1,14 @@
+package ch2;
+
+/*
+    程式實例ch2_3.java
+    作者：DC
+ */
+public class Ch2_3 {
+    // main是程式的起點
+    public static void main(String[] args) {
+        // 字串輸出
+        System.out.println("Hello World");
+        System.out.println("I love Java");      // 第2筆字串輸出
+    }
+}
---
+++ src/ch2/Ch2_4.java
@@ -0,0 +1,14 @@
+package ch2;
+
+/**
+ *  exercise : ch2_4.java
+ *  author : TR Chiu
+ */
+public class Ch2_4 {
+    // main是程式的起點
+    public static void main(String[] args) {
+        // 字串輸出
+        System.out.println("Hello! Java");
+        System.out.println("I love Java");      // 第2筆字串輸出
+    }
+}
